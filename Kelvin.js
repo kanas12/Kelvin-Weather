a523d60c3fe5cc61448e2dff5083d135f5d51487
Daniel Frimpong <daniel.frimpong@amlitech.org>
2021-08-03 13:28:37 +0000

    first commit

---
+++ Kelvin.js
@@ -0,0 +1,10 @@
+// const = 'kelvin';
+const kelvin = 0;
+//
+const celsius = kelvin - 273;
+//
+let fahrenheit = celsius * (9/5) + 32;
+//
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
